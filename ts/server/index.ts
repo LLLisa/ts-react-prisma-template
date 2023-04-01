67a98c8de360eea234edf4d7f6818c32d7cf7056
Lisa Knox <lisaknox83@gmail.com>
2023-03-31 21:18:58 -0400

    seeding works

--- ts/server/index.ts
+++ ts/server/index.ts
@@ -3,3 +3,63 @@ import app from './api';
 const PORT = process.env.port || 1956;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+// //prisma
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// async function dbSeed() {
+//   try {
+//     const alice = await prisma.user.findUnique({
+//       where: {
+//         email: 'alice@prisma.io',
+//       },
+//     });
+//     // if (!alice) {
+//     await prisma.user.create({
+//       data: {
+//         name: 'Aliceeeeeeeeeeeeeeeeeeeeeeeee',
+//         email: 'alice@prisma.io',
+//         posts: {
+//           create: { title: 'Hello World' },
+//         },
+//         profile: {
+//           create: { bio: 'I like turtles' },
+//         },
+//       },
+//     });
+//     // }
+
+//     const allUsers = await prisma.user.findMany({
+//       include: {
+//         posts: true,
+//         profile: true,
+//       },
+//     });
+//     console.dir(allUsers, { depth: null });
+
+//     const post = await prisma.post.update({
+//       where: { id: 1 },
+
+//       data: { published: false },
+//     });
+
+//     console.log(post);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// const dbSync = async () => {
+//   try {
+//     dbSeed();
+//     await prisma.$disconnect;
+//   } catch (error) {
+//     console.error(error);
+//     await prisma.$disconnect;
+//     process.exit(1);
+//   }
+// };
+
+// dbSync();
